dc8e1587f204d611b4259aaa8d5121ae0f0785c3
Jared Blumer <jaredgblumer@gmail.com>
2019-12-09 14:31:12 -0500

    Move files from client to root folder

--- client/src/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
- ynab: {
-   "clientId": "02328a8ebb4f8d0a964f6090e9eeb356e913226a395ca44287dade8c613cd8b9",
-   "redirectUri": "http://localhost:8080"
- }
-};
-
-module.exports = config;
