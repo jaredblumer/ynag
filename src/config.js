a3be8468d7addeb33ee864f6ec771ab42c6dce9b
Jared Blumer <jaredgblumer@gmail.com>
2019-12-10 15:54:38 -0500

    Add Heroku config var

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
  ynab: {
    "clientId": "02328a8ebb4f8d0a964f6090e9eeb356e913226a395ca44287dade8c613cd8b9",
-   "redirectUri": "https://ynag.herokuapp.com"
+   "redirectUri": process.env.REDIRECT_URI || "http://localhost:8080/"
  }
 };
 
